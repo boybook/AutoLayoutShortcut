e570486c50871425d9a6ad39048e515e8a20c137
boybook <fangyizhou1997@qq.com>
2022-03-17 23:05:09 +0800

    Some amazing optimizing for Switch Layout Mode

--- code.js
+++ code.js
@@ -3,25 +3,77 @@
 // This file holds the main code for the plugins. It has access to the *document*.
 // You can access browser APIs such as the network by creating a UI which contains
 // a full browser environment (see documentation).
-// for (let node of figma.currentPage.selection) {
-//   console.log(node.type);
-//   if (isConstraintsLike(node)) {
-//     console.log(node.constraints);
-//   }
-// }
 switch (figma.command) {
+    // Frame
+    // 横向：
+    // - Hug: layoutAlign=INHERIT layoutGrow=0 primaryAxisAlignItems=MIN primaryAxisSizingMode=AUTO
+    // - Fill: layoutAlign=STRETCH layoutGrow=0 primaryAxisAlignItems=MIN primaryAxisSizingMode=FIXED
+    // - Fixed: layoutAlign=INHERIT layoutGrow=0 primaryAxisAlignItems=MIN primaryAxisSizingMode=FIXED
+    // 纵向：
+    // - Hug: layoutAlign=INHERIT layoutGrow=0 primaryAxisAlignItems=MIN primaryAxisSizingMode=AUTO counterAxisSizingMode=AUTO
+    // - Fill: layoutAlign=INHERIT layoutGrow=1 primaryAxisAlignItems=MIN primaryAxisSizingMode=AUTO counterAxisSizingMode=FIXED
+    // - Fixed: layoutAlign=INHERIT layoutGrow=0 primaryAxisAlignItems=MIN primaryAxisSizingMode=AUTO counterAxisSizingMode=FIXED
+    //
+    // case "test": {
+    //   for (let node of figma.currentPage.selection) {
+    //     if (isFrameLike(node)) {
+    //       console.log("layoutAlign=" + node.layoutAlign + " layoutGrow=" + node.layoutGrow + " primaryAxisAlignItems=" + node.primaryAxisAlignItems + " primaryAxisSizingMode=" + node.primaryAxisSizingMode + " counterAxisSizingMode=" + node.counterAxisSizingMode);
+    //     } else if (node.type === 'TEXT') {
+    //       console.log("layoutAlign=" + node.layoutAlign + " layoutGrow=" + node.layoutGrow + " textAutoResize=" + node.textAutoResize);
+    //     }
+    //   }
+    //   break;
+    // }
+    // layoutAlign=STRETCH layoutGrow=0 primaryAxisAlignItems=MIN primaryAxisSizingMode=FIXED counterAxisSizingMode=AUTO
+    //// layoutAlign=STRETCH layoutGrow=0 primaryAxisAlignItems=MIN primaryAxisSizingMode=FIXED counterAxisSizingMode=FIXED
+    // layoutAlign=INHERIT layoutGrow=1 primaryAxisAlignItems=MIN primaryAxisSizingMode=FIXED counterAxisSizingMode=AUTO
+    // layoutAlign=INHERIT layoutGrow=1 primaryAxisAlignItems=MIN primaryAxisSizingMode=AUTO counterAxisSizingMode=FIXED
+    // layoutAlign=STRETCH layoutGrow=0 primaryAxisAlignItems=MIN primaryAxisSizingMode=AUTO counterAxisSizingMode=FIXED
     case "layout_mode": {
         for (let node of figma.currentPage.selection) {
             if (isFrameLike(node)) {
+                for (let child of node.children) {
+                    if (isFrameLike(child)) {
+                        const oldPrimaryAxisSizingMode = child.primaryAxisSizingMode;
+                        const oldCounterAxisSizingMode = child.counterAxisSizingMode;
+                        setTimeout(() => {
+                            if (isFrameLike(child)) {
+                                //console.log("oldPrimaryAxisSizingMode=" + oldPrimaryAxisSizingMode + " oldCounterAxisSizingMode=" + oldCounterAxisSizingMode);
+                                child.layoutAlign = child.layoutAlign === "INHERIT" ? "STRETCH" : "INHERIT";
+                                child.layoutGrow = child.layoutGrow === 0 ? 1 : 0;
+                                child.primaryAxisSizingMode = oldPrimaryAxisSizingMode;
+                                child.counterAxisSizingMode = oldCounterAxisSizingMode;
+                            }
+                        }, 2);
+                    }
+                    else if (child.type === 'TEXT') {
+                        const nodeText = child;
+                        Promise.all(nodeText.getRangeAllFontNames(0, nodeText.characters.length)
+                            .map(figma.loadFontAsync)).then(() => {
+                            setTimeout(() => {
+                                nodeText.layoutAlign = 'INHERIT';
+                                nodeText.textAutoResize = 'WIDTH_AND_HEIGHT';
+                            }, 3);
+                        }).catch(e => {
+                            figma.notify(e, { error: true });
+                        });
+                    }
+                }
+                const oldPrimaryAxisSizingMode = node.primaryAxisSizingMode;
+                const oldCounterAxisSizingMode = node.counterAxisSizingMode;
+                //console.log("oldPrimaryAxisSizingMode=" + oldPrimaryAxisSizingMode + " oldCounterAxisSizingMode=" + oldCounterAxisSizingMode);
+                setTimeout(() => {
+                    if (isFrameLike(node)) {
+                        node.primaryAxisSizingMode = oldCounterAxisSizingMode;
+                        node.counterAxisSizingMode = oldPrimaryAxisSizingMode;
+                    }
+                }, 1);
                 if (node.layoutMode === "HORIZONTAL") {
                     node.layoutMode = "VERTICAL";
                 }
                 else if (node.layoutMode === "VERTICAL") {
                     node.layoutMode = "HORIZONTAL";
                 }
-                const tmp = node.primaryAxisSizingMode;
-                node.primaryAxisSizingMode = node.counterAxisSizingMode;
-                node.counterAxisSizingMode = tmp;
             }
         }
         break;
@@ -86,15 +138,6 @@ switch (figma.command) {
         }
         break;
     }
-    // Frame
-    // 横向：
-    // - Hug: layoutAlign=INHERIT layoutGrow=0 primaryAxisAlignItems=MIN primaryAxisSizingMode=AUTO
-    // - Fill: layoutAlign=STRETCH layoutGrow=0 primaryAxisAlignItems=MIN primaryAxisSizingMode=FIXED
-    // - Fixed: layoutAlign=INHERIT layoutGrow=0 primaryAxisAlignItems=MIN primaryAxisSizingMode=FIXED
-    // 纵向：
-    // - Hug: layoutAlign=INHERIT layoutGrow=0 primaryAxisAlignItems=MIN primaryAxisSizingMode=AUTO counterAxisSizingMode=AUTO
-    // - Fill: layoutAlign=INHERIT layoutGrow=1 primaryAxisAlignItems=MIN primaryAxisSizingMode=AUTO counterAxisSizingMode=FIXED
-    // - Fixed: layoutAlign=INHERIT layoutGrow=0 primaryAxisAlignItems=MIN primaryAxisSizingMode=AUTO counterAxisSizingMode=FIXED
     case "layout_align_primary_fill": {
         for (let node of figma.currentPage.selection) {
             if (node.type === 'FRAME') {
@@ -148,7 +191,7 @@ switch (figma.command) {
         break;
     }
 }
-figma.closePlugin();
+setTimeout(() => figma.closePlugin(), 100);
 function isFrameLike(node) {
     return node.type === "FRAME" || node.type === "COMPONENT" || node.type === "INSTANCE";
 }
